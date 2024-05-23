75c52a746633142c4e8650eae2e5f39d28db03d3
Gabor Vizi <Gabor_Vizi@mentor.com>
2024-05-23 23:51:36 +0200

    Refactor - 2. - calculated field handling with more beautiful solution - Fix 1.

--- mati_db.py
+++ mati_db.py
@@ -197,7 +197,7 @@ def update_menetrend_with_arrive_minutes(result):
     """ Add new column with arrive minute """
     new_result = []
     for item in result:
-        new_item = item + (get_next_arrive(item),)  # Add arrive_minute to last field/column
+        new_item = item['arrive_minute'] = get_next_arrive(item)  # Add a calculated field
         new_result.append(new_item)
     return new_result
 
--- test_local_server.sh
+++ test_local_server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 if [ -z "${DB_PASSWORD}"]; then
-    echo "[ERROR] DB_PASSWORD variable has not set! Exit!"
-    exit 1
+    echo "[ERROR] DB_PASSWORD variable has not set! For 'start_local_server.sh' it is necessary!"
 fi
-curl 127.0.0.1:5000/menetrend?jarat=3
-curl 127.0.0.1:5000/nyomtatas?megallo="Bolya"
-curl 127.0.0.1:5000/bus?line=0
+
+echo curl 127.0.0.1:5000/menetrend?jarat=3
+echo curl 127.0.0.1:5000//menetrend?megallo=L%C3%A1rm%C3%A1kl%20utca
+echo curl 127.0.0.1:5000/nyomtatas?megallo="Bolya"
+echo curl 127.0.0.1:5000/bus?line=0
