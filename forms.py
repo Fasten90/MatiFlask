e14a9c2ee294cf493414887384169fef9e5bd3de
Fasten90 <fasten90@gmail.com>
2024-12-14 15:01:06 +0100

    Line edit - 5. - fixes

--- forms.py
+++ forms.py
@@ -6,6 +6,17 @@ from wtforms.validators import DataRequired
 
 class MatiAdatbazisFeltoltes(FlaskForm):
     # Result not needed to here
+    jarat = StringField('Járat (szám)', validators=[DataRequired()])
+    min_hour = IntegerField('min_hour', validators=[DataRequired()])
+    max_hour = IntegerField('max_hour', validators=[DataRequired()])
+    jaratsuruseg_minute = IntegerField('Járatsűrűség hétköznap', validators=[DataRequired()])
+    start_minute = IntegerField('Indulási időpont', validators=[DataRequired()])
+    station = StringField('Megálló (station)', validators=[DataRequired()])
+    jarat_tipus = SelectField('Járat típus', choices=['VCAF', 'VCOM', 'VGANZ', 'VTATRA', 'M', 'H', 'BUSZ', 'BUSZTROLI', 'ÉJSZAKAI', 'VOLÁNBUSZ', 'DHAJO', 'VONAT'])
+    jaratsuruseg_hetvege = IntegerField('Járatsűrűség hétvégén', 0)
+    #varos - not used, use de default
+    low_floor = StringField('Alacsonypadlós: (alacsonypadlósok száma/magas padlósok száma arányban kell beírni, pl. "2_1")')
+    is_edit = BooleanField('is_edit', default=False)
 
     def __init__(self, default_values):
         super().__init__()
